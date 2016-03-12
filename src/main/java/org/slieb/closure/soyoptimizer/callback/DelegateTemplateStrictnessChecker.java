b5f629128e5a7c1a2c4e1fbda40542b0bc3d3a26
Stefan Liebenberg <siga.fredo@gmail.com>
2016-03-12 09:35:01 +0100

    Adding Strictness checker

---
+++ src/main/java/org/slieb/closure/soyoptimizer/callback/DelegateTemplateStrictnessChecker.java
@@ -0,0 +1,24 @@
+package org.slieb.closure.soyoptimizer.callback;
+
+import com.google.javascript.jscomp.NodeTraversal;
+import com.google.javascript.rhino.Node;
+import org.slieb.closure.soyoptimizer.SoyDelegateNodeHelper;
+import org.slieb.closure.soyoptimizer.node.DelegateGetIdentifierCall;
+
+import static org.slieb.closure.soyoptimizer.SoyDiagnostics.SOY_NOT_STRING_LITERAL_FOR_ID;
+
+public class DelegateTemplateStrictnessChecker extends NodeTraversal.AbstractPostOrderCallback {
+
+    @Override
+    public void visit(final NodeTraversal t,
+                      final Node n,
+                      final Node parent) {
+
+        if (SoyDelegateNodeHelper.isGetDelegateIdCall(n)) {
+            final Node templateName = new DelegateGetIdentifierCall(n).getTemplateNode().get();
+            if (!templateName.isString()) {
+                t.report(templateName, SOY_NOT_STRING_LITERAL_FOR_ID);
+            }
+        }
+    }
+}
