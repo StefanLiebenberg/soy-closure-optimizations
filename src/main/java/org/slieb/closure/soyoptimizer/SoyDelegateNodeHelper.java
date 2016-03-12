5bef065867ab0674ee0c96bbeb390e26267a7f8a
Stefan Liebenberg <siga.fredo@gmail.com>
2016-03-12 09:26:51 +0100

    adding get register call

--- src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
+++ src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
@@ -8,12 +8,22 @@ public class SoyDelegateNodeHelper {
 
     public static final String GET_DELEGATE_ID = "soy.$$getDelTemplateId";
     public static final String GET_DELEGATE_FN = "soy.$$getDelegateFn";
+    public static final String REGISTER_DELEGATE_FN = "soy.$$registerDelegateFn";
+
+    public static boolean isRegisterDelegateFunction(Node node) {
+        return isMethodCall(node, REGISTER_DELEGATE_FN);
+    }
 
     public static boolean isGetDelegateIdCall(final Node n) {
-        return n.isCall() && n.getFirstChild().matchesQualifiedName(GET_DELEGATE_ID);
+        return isMethodCall(n, GET_DELEGATE_ID);
     }
 
     public static boolean isGetDelegateFnCall(final Node n) {
-        return n.isCall() && n.getFirstChild().matchesQualifiedName(GET_DELEGATE_FN);
+        return isMethodCall(n, GET_DELEGATE_FN);
+    }
+
+    private static boolean isMethodCall(final Node n,
+                                        final String methodName) {
+        return n.isCall() && n.getFirstChild().matchesQualifiedName(methodName);
     }
 }
