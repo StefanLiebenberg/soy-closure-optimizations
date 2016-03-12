15efd5c93c5c5293f7af2b1ecc0c19b95259ce0f
Stefan Liebenberg <siga.fredo@gmail.com>
2016-03-12 09:31:59 +0100

    Adding DelegateTemplateRegistrationsNode

--- src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
+++ src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
@@ -9,6 +9,7 @@ public class SoyDelegateNodeHelper {
     public static final String GET_DELEGATE_ID = "soy.$$getDelTemplateId";
     public static final String GET_DELEGATE_FN = "soy.$$getDelegateFn";
     public static final String REGISTER_DELEGATE_FN = "soy.$$registerDelegateFn";
+    public static final String SOY_TEMPLATE_NAME_PROPERTY = "soyTemplateName";
 
     public static boolean isRegisterDelegateFunction(Node node) {
         return isMethodCall(node, REGISTER_DELEGATE_FN);
@@ -26,4 +27,9 @@ public class SoyDelegateNodeHelper {
                                         final String methodName) {
         return n.isCall() && n.getFirstChild().matchesQualifiedName(methodName);
     }
+
+    public static boolean isSoyTemplateNameProperty(Node node) {
+        return node.getLastChild().getString().equals(SOY_TEMPLATE_NAME_PROPERTY);
+    }
+
 }
---
+++ src/main/java/org/slieb/closure/soyoptimizer/callback/DelegateTemplateRegistrationsRemover.java
@@ -0,0 +1,75 @@
+package org.slieb.closure.soyoptimizer.callback;
+
+import com.google.javascript.jscomp.NodeTraversal;
+import com.google.javascript.rhino.Node;
+import com.google.javascript.rhino.Token;
+import org.slieb.closure.soyoptimizer.DelegateRegistration;
+import org.slieb.closure.soyoptimizer.node.DelegateRegistrationCall;
+
+import java.util.Set;
+
+import static org.slieb.closure.soyoptimizer.SoyDelegateNodeHelper.isRegisterDelegateFunction;
+import static org.slieb.closure.soyoptimizer.SoyDelegateNodeHelper.isSoyTemplateNameProperty;
+
+/**
+ * A soy visitor that removes all soy registrations that match any element in a given set of DelegateRegistrations.
+ */
+public class DelegateTemplateRegistrationsRemover extends NodeTraversal.AbstractShallowCallback {
+
+    private final Set<DelegateRegistration> registrations;
+
+    public DelegateTemplateRegistrationsRemover(final Set<DelegateRegistration> registrations) {
+        this.registrations = registrations;
+    }
+
+    @Override
+    public void visit(final NodeTraversal t,
+                      final Node n,
+                      final Node parent) {
+        if (nodeIsRemovableRegistration(n) || nodeIsRemovableReference(n)) {
+            detachAndClean(n, parent);
+            t.getCompiler().reportCodeChange();
+        }
+    }
+
+    private boolean nodeIsRemovableRegistration(final Node n) {
+        return isRegisterDelegateFunction(n) &&
+                registrations.stream().anyMatch(new DelegateRegistrationCall(n).toRegistration()::equals);
+    }
+
+    private boolean nodeIsRemovableReference(final Node n) {
+        return n.isAssign() && (assignNodeIsRemovableTemplateMethodAssign(n) || assignNodeIsRemovableDelegateTemplateNameSetter(n));
+    }
+
+    private boolean assignNodeIsRemovableDelegateTemplateNameSetter(final Node n) {
+        return n.getLastChild().isString() && isSoyTemplateNameProperty(n.getFirstChild()) && isRemovableMethodName(n.getLastChild().getString());
+    }
+
+    private boolean assignNodeIsRemovableTemplateMethodAssign(final Node n) {
+        return n.getLastChild().isFunction() && n.getFirstChild().isGetProp() && isRemovableMethodName(n.getFirstChild().getQualifiedName());
+    }
+
+    private void detachAndClean(Node n,
+                                Node parent) {
+        n.detachFromParent();
+        if (shouldDetachParent(parent)) {
+            detachAndClean(parent, parent.getParent());
+        }
+    }
+
+    private boolean shouldDetachParent(Node parent) {
+        switch (parent.getType()) {
+            case Token.BLOCK:
+            case Token.EXPR_RESULT:
+                return !parent.hasChildren();
+            case Token.IF:
+                return (parent.getChildCount() <= 1);
+            default:
+                return false;
+        }
+    }
+
+    private boolean isRemovableMethodName(final String methodName) {
+        return registrations.stream().map(DelegateRegistration::getMethodName).anyMatch(methodName::equals);
+    }
+}
