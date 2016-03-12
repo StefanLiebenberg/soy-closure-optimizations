7ffbe282f63a737f52f95ec2b80b88304296836f
Stefan Liebenberg <siga.fredo@gmail.com>
2016-03-12 09:23:53 +0100

    adding get delegate fn getter

--- src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
+++ src/main/java/org/slieb/closure/soyoptimizer/SoyDelegateNodeHelper.java
@@ -7,8 +7,13 @@ public class SoyDelegateNodeHelper {
     private SoyDelegateNodeHelper() {}
 
     public static final String GET_DELEGATE_ID = "soy.$$getDelTemplateId";
+    public static final String GET_DELEGATE_FN = "soy.$$getDelegateFn";
 
     public static boolean isGetDelegateIdCall(final Node n) {
         return n.isCall() && n.getFirstChild().matchesQualifiedName(GET_DELEGATE_ID);
+    }
+
+    public static boolean isGetDelegateFnCall(final Node n) {
+        return n.isCall() && n.getFirstChild().matchesQualifiedName(GET_DELEGATE_FN);
     }
 }
